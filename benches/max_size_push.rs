996d3102f2d8cbe0552d9715272532e9e2a0b2e5
Markus Reiter <me@reitermark.us>
2018-12-03 05:33:50 +0100

    Add benchmark.

---
+++ benches/max_size_push.rs
@@ -0,0 +1,19 @@
+#[macro_use]
+extern crate criterion;
+use criterion::Criterion;
+
+extern crate medianheap;
+use medianheap::MedianHeap;
+
+fn benchmark(c: &mut Criterion) {
+  c.bench_function("push with max_size", |b| b.iter(|| {
+    let mut heap = MedianHeap::with_max_size(512);
+
+    for i in (0..8192).chain((0..8192).rev()) {
+      heap.push(i);
+    }
+  }));
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
